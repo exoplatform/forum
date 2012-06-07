64e2400af83579a450770ce7e89c41c898f4d222
tu-vu-duy <tuvd@exoplatform.com>
2012-06-07 18:24:13 +0700

    Change Artifact Id

--- packaging/module/src/main/javascript/forum.packaging.module.js
+++ packaging/module/src/main/javascript/forum.packaging.module.js
@@ -32,12 +32,12 @@ function getModule(params) {
   
   // FORUM components
   module.component = {};
-  module.component.common = new Project("org.exoplatform.forum", "exo.forum.component.common", "jar", module.version).
+  module.component.common = new Project("org.exoplatform.forum", "exo-forum-component-common", "jar", module.version).
                             addDependency(new Project("org.exoplatform.commons", "exo.platform.commons.webui", "jar", commonsVersion));
-  module.component.rendering = new Project("org.exoplatform.forum", "exo.forum.component.rendering", "jar", module.version).
-                            addDependency(new Project("org.exoplatform.forum", "exo.forum.component.macro.iframe", "jar", module.version)).
-                            addDependency(new Project("org.exoplatform.forum", "exo.forum.component.macro.jira", "jar", module.version));
-  module.component.bbcode = new Project("org.exoplatform.forum", "exo.forum.component.bbcode", "jar", module.version);
+  module.component.rendering = new Project("org.exoplatform.forum", "exo-forum-component-rendering", "jar", module.version).
+                            addDependency(new Project("org.exoplatform.forum", "exo-forum-component-macro-iframe", "jar", module.version)).
+                            addDependency(new Project("org.exoplatform.forum", "exo-forum-component-macro-jira", "jar", module.version));
+  module.component.bbcode = new Project("org.exoplatform.forum", "exo-forum-component-bbcode", "jar", module.version);
 
   module.component.upgrade = new Project("org.exoplatform.commons", "exo.platform.commons.component.upgrade", "jar", commonsVersion).
     addDependency(new Project("org.exoplatform.commons", "exo.platform.commons.component.product", "jar", commonsVersion));
@@ -45,67 +45,67 @@ function getModule(params) {
   
   // FORUM application common
   module.application = {};
-  module.application.common = new Project("org.exoplatform.forum", "exo.forum.application.common", "jar", module.version).
-    addDependency(new Project("org.exoplatform.forum", "exo.forum.component.upgrade", "jar",  module.version));
-  module.application.forumGadgets = new Project("org.exoplatform.forum", "exo.forum.gadgets", "war", module.version);
+  module.application.common = new Project("org.exoplatform.forum", "exo-forum-application-common", "jar", module.version).
+    addDependency(new Project("org.exoplatform.forum", "exo-forum-component-upgrade", "jar",  module.version));
+  module.application.forumGadgets = new Project("org.exoplatform.forum", "exo-forum-gadgets", "war", module.version);
   module.application.forumGadgets.deployName = "forum-gadgets";
 
   // FORUM
-  module.forum = new Project("org.exoplatform.forum", "exo.forum.webapp", "war", module.version).
+  module.forum = new Project("org.exoplatform.forum", "exo-forum-webapp", "war", module.version).
     addDependency(ws.frameworks.json).
-    addDependency(new Project("org.exoplatform.forum", "exo.forum.service", "jar",  module.version));
+    addDependency(new Project("org.exoplatform.forum", "exo-forum-service", "jar",  module.version));
   module.forum.deployName = "forum";
 
   
   // Answer
   module.answer =
-    new Project("org.exoplatform.forum", "exo.answer.webapp", "war", module.version).
-    addDependency(new Project("org.exoplatform.forum", "exo.answer.service", "jar",  module.version));
+    new Project("org.exoplatform.forum", "exo-answer-webapp", "war", module.version).
+    addDependency(new Project("org.exoplatform.forum", "exo-answer-service", "jar",  module.version));
   module.answer.deployName = "answer";
 
 
   // POLL
   module.poll = 
-    new Project("org.exoplatform.forum", "exo.poll.webapp", "war", module.version) .
-    addDependency(new Project("org.exoplatform.forum", "exo.poll.service", "jar",  module.version));
+    new Project("org.exoplatform.forum", "exo-poll-webapp", "war", module.version) .
+    addDependency(new Project("org.exoplatform.forum", "exo-poll-service", "jar",  module.version));
   module.poll.deployName = "poll";
 
   // FORUM we resources and services
   module.web = {}
   module.web.forumResources = 
-    new Project("org.exoplatform.forum", "exo.forum.web.forumResources", "war", module.version) ;
+    new Project("org.exoplatform.forum", "exo-forum-forumResources", "war", module.version) ;
 
   // FORUM extension for tomcat
   module.extension = {};
   module.extension.webapp = 
-    new Project("org.exoplatform.forum", "exo.forum.extension.webapp", "war", module.version);
+    new Project("org.exoplatform.forum", "exo-forum-extension-webapp", "war", module.version);
   module.extension.webapp.deployName = "forum-extension";
    
   module.server = {}
   module.server.tomcat = {}
   module.server.tomcat.patch =
-    new Project("org.exoplatform.forum", "exo.forum.server.tomcat.patch", "jar", module.version);
+    new Project("org.exoplatform.forum", "exo-forum-server-tomcat-patch", "jar", module.version);
 	
   module.server.jboss = {}
   module.server.jboss.patchear =
-    new Project("org.exoplatform.forum", "exo.forum.server.jboss.patch-ear", "jar", module.version);
+    new Project("org.exoplatform.forum", "exo-forum-server-jboss-patch-ear", "jar", module.version);
    
   // FORUM demo 
   module.demo = {};
   // demo portal
   module.demo.portal =
-    new Project("org.exoplatform.forum", "exo.forum.demo.webapp", "war", module.version).
-    addDependency(new Project("org.exoplatform.forum", "exo.forum.component.injector", "jar", module.version)).
-    addDependency(new Project("org.exoplatform.forum", "exo.forum.demo.config", "jar", module.version));
+    new Project("org.exoplatform.forum", "exo-forum-demo-webapp", "war", module.version).
+    addDependency(new Project("org.exoplatform.forum", "exo-forum-component-injector", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.forum", "exo-forum-demo-config", "jar", module.version));
   module.demo.portal.deployName = "forumdemo";  
 	
   module.demo.cometd=
-    new Project("org.exoplatform.forum", "exo.forum.demo.cometd-war", "war", module.version);
+    new Project("org.exoplatform.forum", "exo-forum-demo-cometd-war", "war", module.version);
   module.demo.cometd.deployName = "cometd-forumdemo";
 	   
   // demo rest endpoint	   
   module.demo.rest =
-    new Project("org.exoplatform.forum", "exo.forum.demo.rest-forumdemo", "war", module.version).
+    new Project("org.exoplatform.forum", "exo-forum-demo-rest-forumdemo", "war", module.version).
     addDependency(ws.frameworks.servlet);
   module.extension.deployName = "rest-forumdemo"; 
    
