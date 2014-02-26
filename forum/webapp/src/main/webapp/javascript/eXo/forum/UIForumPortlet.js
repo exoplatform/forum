283b8cd0e4e730f79ec11c61e9c7a9392f047ad4
TRAN Trung Thanh <thanhtt@exoplatform.com>
2014-02-26 17:41:54 +0700

    FORUM-804: [IE8] Can not jump to the last reply when click on 'View the latest reply'
    Fix description:
    	- In IE, use scrollTo to scroll to specific item

--- forum/webapp/src/main/webapp/javascript/eXo/forum/UIForumPortlet.js
+++ forum/webapp/src/main/webapp/javascript/eXo/forum/UIForumPortlet.js
@@ -439,8 +439,18 @@
           }
         } else {
           var obj = document.getElementById(idLastPost);
-          if (obj)
-            obj.scrollIntoView(true);
+          if (eXo.core.Browser.isIE()) {
+            var correctOffset = 0;
+            if (obj.offsetParent) {
+              do {
+                correctOffset += obj.offsetTop;
+              } while (obj = obj.offsetParent);
+            }
+            $("html, body").scrollTop(correctOffset);
+          } else {
+            if (obj)
+              obj.scrollIntoView(true);
+          }
         }
       }
     },
