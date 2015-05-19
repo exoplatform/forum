5b8153d84925997dd5d8fd88d4ce3c2042bfecbe
tu-vu-duy <tuvd@exoplatform.com>
2015-05-19 10:38:27 +0700

    FORUM-1117 | [TC-PERF-PLF-4.2-RC1][INTRANET_FORUM_READ_TOPIC] Slowness in UITopicDetail.run

--- forum/webapp/src/main/java/org/exoplatform/forum/webui/UITopicDetail.java
+++ forum/webapp/src/main/java/org/exoplatform/forum/webui/UITopicDetail.java
@@ -567,7 +567,7 @@ public class UITopicDetail extends UIForumKeepStickPageIterator {
 
   protected List<Post> getPostPageList() throws Exception {
     Post[] posts = null;
-    
+    mapUserProfile.clear();
     int pageSize = (int)getUserProfile().getMaxPostInPage();
     try {
       try {
@@ -598,7 +598,6 @@ public class UITopicDetail extends UIForumKeepStickPageIterator {
       pagePostRemember.put(topicId, pageSelect);
 
       List<String> userNames = new ArrayList<String>();
-      mapUserProfile.clear();
       for (Post post : posts) {
         if (!userNames.contains(post.getOwner()))
           userNames.add(post.getOwner());
@@ -618,17 +617,6 @@ public class UITopicDetail extends UIForumKeepStickPageIterator {
         userProfile.addLastPostIdReadOfTopic(topicId, IdLastPost);
         if (!UserProfile.USER_GUEST.equals(userName)) {
           getForumService().saveLastPostIdRead(userName, userProfile.getLastReadPostOfForum(), userProfile.getLastReadPostOfTopic());
-        }
-      }
-      // updateUserProfiles
-      if (userNames.size() > 0) {
-        try {
-          List<UserProfile> profiles = getForumService().getQuickProfiles(userNames);
-          for (UserProfile profile : profiles) {
-            mapUserProfile.put(profile.getUserId(), profile);
-          }
-        } catch (Exception e) {
-          log.warn("Failed to load qui profiles: " + e.getMessage(), e);
         }
       }
     } catch (Exception e) {
