7bcfa29d147a3c3910604097d62098b19148c677
smartytwiti <marwen109@gmail.com>
2016-05-30 17:20:52 +0100

    FORUM-1251 : merge all forum wars into forum.war

--- forum/webapp/src/main/webapp/eXoPlugins/helpBBCode/plugin.js
+++ forum/webapp/src/main/webapp/eXoPlugins/helpBBCode/plugin.js
@@ -20,5 +20,5 @@ CKEDITOR.plugins.add('helpBBCode',
 );
 
 function showHelpBBCode(e){   
-  window.open('/forumResources/eXoPlugins/helpBBCode/helpBBCode.html','helpBBCode','width=800,height=600');
+  window.open('/forum/eXoPlugins/helpBBCode/helpBBCode.html','helpBBCode','width=800,height=600');
 }
