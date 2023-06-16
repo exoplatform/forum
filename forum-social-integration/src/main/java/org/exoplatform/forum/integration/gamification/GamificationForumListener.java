7203ab76735dd6fa314731615328111145f85b9a
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-06-16 09:49:01 +0200

    fix: Fix Gamification API package name (#202)

--- forum-social-integration/src/main/java/org/exoplatform/forum/integration/gamification/GamificationForumListener.java
+++ forum-social-integration/src/main/java/org/exoplatform/forum/integration/gamification/GamificationForumListener.java
@@ -1,13 +1,13 @@
 package org.exoplatform.forum.integration.gamification;
 
 import java.beans.PropertyChangeEvent;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.exoplatform.addons.gamification.GamificationConstant;
-import org.exoplatform.addons.gamification.service.configuration.RuleService;
-import org.exoplatform.addons.gamification.service.effective.GamificationService;
 import org.exoplatform.forum.service.*;
 import org.exoplatform.services.listener.Asynchronous;
+import org.exoplatform.services.listener.ListenerService;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
@@ -26,28 +26,24 @@ public class GamificationForumListener extends ForumEventListener {
 
   private static final String   GAMIFICATION_FORUM_VOTE_TOPIC = "voteForumTopic";
 
-  protected RuleService         ruleService;
-
   protected IdentityManager     identityManager;
 
   protected SpaceService        spaceService;
 
-  protected GamificationService gamificationService;
+  protected ListenerService     listenerService;
 
   protected ActivityManager     activityManager;
 
   protected ForumService        forumService;
 
-  public GamificationForumListener(RuleService ruleService,
-                                   IdentityManager identityManager,
+  public GamificationForumListener(IdentityManager identityManager,
                                    SpaceService spaceService,
-                                   GamificationService gamificationService,
+                                   ListenerService listenerService,
                                    ActivityManager activityManager,
                                    ForumService forumService) {
-    this.ruleService = ruleService;
     this.identityManager = identityManager;
     this.spaceService = spaceService;
-    this.gamificationService = gamificationService;
+    this.listenerService = listenerService;
     this.activityManager = activityManager;
     this.forumService = forumService;
   }
@@ -64,12 +60,16 @@ public class GamificationForumListener extends ForumEventListener {
   public void addPost(Post post) {
 
     String actorId = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, post.getOwner(), false).getId();
-    gamificationService.createHistory(GAMIFICATION_FORUM_ADD_POST,
-                                      actorId,
-                                      actorId,
-                                      ForumUtils.createdSubForumLink(post.toString(), post.getTopicId(), true),
-                                      GamificationConstant.IDENTITY_OBJECT_TYPE);
-
+    try {
+      Map<String, String> gamificationRule = new HashMap<>();
+      gamificationRule.put("ruleTitle", GAMIFICATION_FORUM_ADD_POST);
+      gamificationRule.put("senderId", actorId);
+      gamificationRule.put("receiverId", actorId);
+      gamificationRule.put("object", ForumUtils.createdSubForumLink(post.toString(), post.getTopicId(), true));
+      listenerService.broadcast("exo.gamification.generic.action", gamificationRule, "");
+    } catch (Exception e) {
+      LOG.error("Cannot broadcast add forum post Gamification event" + e.getMessage(), e);
+    }
   }
 
   @Override
@@ -85,11 +85,16 @@ public class GamificationForumListener extends ForumEventListener {
   public void addTopic(Topic topic) {
 
     String actorId = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, topic.getOwner(), false).getId();
-    gamificationService.createHistory(GAMIFICATION_FORUM_ADD_TOPIC,
-                                      actorId,
-                                      actorId,
-                                      ForumUtils.createdSubForumLink(topic.toString(), topic.getId(), true),
-                                      GamificationConstant.IDENTITY_OBJECT_TYPE);
+    try {
+      Map<String, String> gamificationRule = new HashMap<>();
+      gamificationRule.put("ruleTitle", GAMIFICATION_FORUM_ADD_TOPIC);
+      gamificationRule.put("senderId", actorId);
+      gamificationRule.put("receiverId", actorId);
+      gamificationRule.put("object", ForumUtils.createdSubForumLink(topic.toString(), topic.getId(), true));
+      listenerService.broadcast("exo.gamification.generic.action", gamificationRule, "");
+    } catch (Exception e) {
+      LOG.error("Cannot broadcast add forum topic Gamification event" + e.getMessage(), e);
+    }
 
   }
 
@@ -149,11 +154,16 @@ public class GamificationForumListener extends ForumEventListener {
     // Start gamification process only when a topic is voted
     if (Topic.TOPIC_RATING.equals(event.getPropertyName())) {
       String actorId = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, topic.getOwner(), false).getId();
-      gamificationService.createHistory(GAMIFICATION_FORUM_VOTE_TOPIC,
-                                        actorId,
-                                        actorId,
-                                        ForumUtils.createdSubForumLink(topic.toString(), topic.getId(), true),
-                                        GamificationConstant.IDENTITY_OBJECT_TYPE);
+      try {
+        Map<String, String> gamificationRule = new HashMap<>();
+        gamificationRule.put("ruleTitle", GAMIFICATION_FORUM_VOTE_TOPIC);
+        gamificationRule.put("senderId", actorId);
+        gamificationRule.put("receiverId", actorId);
+        gamificationRule.put("object", ForumUtils.createdSubForumLink(topic.toString(), topic.getId(), true));
+        listenerService.broadcast("exo.gamification.generic.action", gamificationRule, "");
+      } catch (Exception e) {
+        LOG.error("Cannot broadcast add forum topic vote Gamification event" + e.getMessage(), e);
+      }
     }
 
   }
